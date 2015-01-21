9828960ba935e473bd65c80889e4b3cb5e32d475
pinguinson <nikita.penguin@gmail.com>
2015-01-21 12:56:06 +0300

    smth works

--- app/src/main/java/ru/ifmo/md/exam1/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/exam1/MainActivity.java
@@ -1,17 +1,38 @@
 package ru.ifmo.md.exam1;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    List<Task> tasks;
+    TaskAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tasks = new ArrayList<>();
+
+        String[] tags = {"study", "university"};
+        tasks.add(new Task("math", "do math", tags));
+        tasks.add(new Task("android", "do hw", tags));
+        adapter = new TaskAdapter(this, tasks);
+        ListView listView = (ListView) findViewById(R.id.list_tasks);
+        listView.setAdapter(adapter);
     }
 
 
@@ -24,13 +45,24 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+            TextView editTitle = (TextView) findViewById(R.id.edit_title);
+            TextView editDescription = (TextView) findViewById(R.id.edit_description);
+            String newTitle = editTitle.getEditableText().toString();
+            String newDescription = editDescription.getEditableText().toString();
+            Task task = new Task(newTitle, newDescription);
+            tasks.add(task);
+            ContentValues cv = new ContentValues();
+            cv.put(TasksProvider.FIELD_TITLE, task.getTitle());
+            cv.put(TasksProvider.FIELD_DESCRIPTION, task.getDescription());
+            cv.put(TasksProvider.FIELD_DATE, task.getDate());
+            cv.put(TasksProvider.FIELD_TAGS, task.getTagsSingleString());
+            adapter = new TaskAdapter(this, tasks);
+            ListView listView = (ListView) findViewById(R.id.list_tasks);
+            listView.setAdapter(adapter);
+            Toast.makeText(this, "added", Toast.LENGTH_SHORT).show();
             return true;
         }
 
